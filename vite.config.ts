0ad78f6456c524af0dad6935dad48b46ff516d69
Vignesh Iyer <vgnshiyer@gmail.com>
2025-10-05 21:34:52 -0700

    feat: gh-pages config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
+import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/be-a-better-tm/" : "/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
