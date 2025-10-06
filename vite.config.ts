8c05ae5a0646e1de7ab918a6ae88cdddf05ebd9d
Vignesh Iyer <vgnshiyer@gmail.com>
2025-10-05 21:41:41 -0700

    fix: update production base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/be-a-better-tm/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/be-a-better-tm" : "/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
